1b82a7c7a62f05599f01e01d96824900a6fa1665
Akeksandr <sanek.pershin.68rus@gmail.com>
2022-04-25 20:36:37 +0300

    Game-'Amazing-racer'-made-during-the-course

---
+++ AmazingRacer/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+	//A reference to the game manager
+	public GameManager gameManager; 
+
+	// Triggers when the player enters the water
+	void OnTriggerEnter(Collider other)
+	{
+		// Moves the player to the spawn point
+		gameManager.PositionPlayer();
+	}
+}
